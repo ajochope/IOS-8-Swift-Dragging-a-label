ddd271881028f05058d8fd465b14a5fed7c4e5ca
ajochope <ocalles@gmail.com>
2015-02-09 09:23:31 +0100

    IOS 8 Swift Dragging examples
    IOS 8 Swift Dragging examples

--- Dragging/ViewController.swift
+++ Dragging/ViewController.swift
@@ -13,8 +13,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var label:UILabel = UILabel(frame: CGRectMake(self.view.bounds.width / 2 - 100, self.view.bounds.height / 2 - 50, 200, 100))
+        label.text = "Drag Me!"
+        label.textAlignment = NSTextAlignment.Center // align center the text
+        self.view.addSubview(label)
+        
+        // recognizer for the dragging move
+        var gesture = UIPanGestureRecognizer(target: self, action: Selector("wasDragged:"))
+        // get a lot of event all the drag gesture's path
+        label.addGestureRecognizer(gesture)
+        label.userInteractionEnabled = true
+        
     }
 
+    func wasDragged(gesture: UIPanGestureRecognizer) {
+        // translation vector origin -> destination
+        
+        let translation = gesture.translationInView(self.view) // get the translation
+        var label = gesture.view! // the view inside the gesture
+        
+        // move the label with the translation
+        label.center = CGPoint(x: label.center.x + translation.x, y: label.center.y + translation.y)
+        
+        // reset the translation that now, is already applied to the label
+        gesture.setTranslation(CGPointZero, inView: self.view)
+        
+        
+        println("dragged detected!!")
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
